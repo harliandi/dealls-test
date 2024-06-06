964e71fe19feca82a08f1f265e268b0077db89ac
Harliandi <harliandi2704@gmail.com>
2024-06-06 07:30:33 +0700

    [DEALLS-TEST] update auth request

--- request/auth.go
+++ request/auth.go
@@ -3,8 +3,8 @@ package request
 type SignupRequest struct {
 	Name      string `json:"name" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
-	Gender    string `json:"gender" validate:"required,oneof=male female prefer_not_to"` // "male" or "female"
-	BirthDate string `json:"birth_date" validate:"birth-date"`                           // YYYY-MM-DD
+	Gender    string `json:"gender" validate:"required,oneof=male female"` // "male" or "female"
+	BirthDate string `json:"birth_date" validate:"birth-date"`             // YYYY-MM-DD
 	Password  string `json:"password" validate:"required"`
 }
 
